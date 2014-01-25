186150df7300a6c23ad01e08338344a76c2d93ae
Christian Bundy <me@christianbundy.com>
2014-01-25 15:13:51 -0800

    Add author

--- toJson.js
+++ toJson.js
@@ -3,25 +3,37 @@ var fs = require('graceful-fs');
 
 
 var config = {
-  path: './test/' // Experiences
+  input:  './private/',
+  output: './output/'
 };
 
 var Experience = {};
 
 console.log("Let's get started!");
 
-var files = fs.readdirSync(config.path);
+var files = fs.readdirSync(config.input);
 
 for (var i in files) {
   if (files[i][0] !== '.') {
     (function (current) {
-      fs.readFile(config.path + files[current], function (err, html) {
+      fs.readFile(config.input + files[current], function (err, html) {
         if (err) {
-          throw err; 
+          throw err;
         } else {
+          // Configuration
           var doc = html.toString();
           var $ = cheerio.load(doc);
-          console.log($('.author').text());
+          var id = files[current].slice(0, -5);
+
+          Experience[id] = {};
+
+          Experience[id].author = $('.author a').text();
+          // console.log(Experience[id]);
+          fs.writeFile(config.output + id + '.json', JSON.stringify(Experience[id]), function(err) {
+            if(err) {
+              console.log(err);
+            }
+          }); 
         }
       });
     })(i);
