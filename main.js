f31986fc4b7a790f979a65b1edc300921fb900c5
Christian Bundy <me@christianbundy.com>
2014-01-26 11:07:21 -0800

    Remove more prepended spaces

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ var dive = require('dive');      // Recursive dir walking
 
 var main = function(e) {
 	for (var i = 0; i < e.dose.length; i++) {
-		for (var a = 0; a < e.dose[i].length; a++) {
-			if (e.dose[i][a][0] === ' ') {
-				e.dose[i][a] = e.dose[i][a].substring(1)
+		for (key in e.dose[i]) {
+			console.log(e.dose[i][key])
+			if (e.dose[i][key][0] == ' ') {
+
+				e.dose[i][key] = e.dose[i][key].substring(1)
 			}
 		}
 		e.dose[i].time = e.dose[i].time.replace(' T+ ', '');
