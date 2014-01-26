add9b22b3698dad69b3c0557ff4981e32ae03191
Christian Bundy <me@christianbundy.com>
2014-01-25 23:51:23 -0800

    Add boilerplate for more customization

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 var fs = require('graceful-fs'); // Queues fs.readFile()
 var dive = require('dive');      // Recursive dir walking
 
+var main = function(experience) {
+	console.log(experience.id)
+	return experience;
+}
+
 dive(__dirname + '/json', function (err, file) {
 	fs.readFile(file, function (err, contents) {
 		if (err) throw err;
 		var obj = JSON.parse(contents);
-		
-		delete obj.header;
-
+		obj = main(obj);
 		fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
 			if (err) throw err;
 		});
