64618628a9353803347c1a7ae10a57fbe688b990
Christian Bundy <me@christianbundy.com>
2014-01-25 21:38:38 -0800

    Add check for changes

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ dive(__dirname + '/json', function (err, file) {
 	fs.readFile(file, function (err, contents) {
 		if (err) throw err;
 		var obj = JSON.parse(contents);
-		var str = JSON.stringify(obj);
-		obj = JSON.parse(str.replace('\\n', ''));
-		fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
-			if (err) throw err;
-		}); 
+		var original = obj;
+
+		if (obj !== original) {
+			fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
+				if (err) throw err;
+			});
+			console.log('Change!')
+		}
 	})
 });
