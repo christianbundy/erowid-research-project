70459f2d917f91cfd28b8c6223db1d1ad6b64044
Christian Bundy <me@christianbundy.com>
2014-01-25 23:41:35 -0800

    Remove header property

--- main.js
+++ main.js
@@ -6,27 +6,7 @@ dive(__dirname + '/json', function (err, file) {
 		if (err) throw err;
 		var obj = JSON.parse(contents);
 		
-		obj.dose = {};
-
-		if (typeof obj.header !== 'undefined') {
-			if (typeof obj.header[4] !== 'undefined' && obj.header[4][0] === '(') {
-				columns = 5
-			} else {
-				columns = 4
-			}
-			for (var i = 0; i < Math.floor(obj.header.length / columns); i++) {
-				round = i * columns;
-				obj.dose[i] = {
-					time: obj.header[0 + round],
-					amount: obj.header[1 + round],
-					administration: obj.header[2 + round],
-					substance: obj.header[3 + round],
-				};
-				if (columns > 4) {
-					obj.dose[i].form = obj.header[4 + round];
-				}
-			};
-		}
+		delete obj.header;
 
 		fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
 			if (err) throw err;
