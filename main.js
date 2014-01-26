20047c4d27e90f25c6dd7a92217782fb5cb9db21
Christian Bundy <me@christianbundy.com>
2014-01-25 22:06:00 -0800

    Add first dose

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 var fs = require('graceful-fs'); // Queues fs.readFile()
 var dive = require('dive');      // Recursive dir walking
 
-var change = false;
 dive(__dirname + '/json', function (err, file) {
 	fs.readFile(file, function (err, contents) {
 		if (err) throw err;
 		var obj = JSON.parse(contents);
-		var original = obj;
+		obj.dose = [];
 
-		if (obj !== original) {
-			fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
-				if (err) throw err;
-			});
-			if (!change) {
-				change=true;
-				console.log('Change!')
-			}
+		if (typeof obj.header[0] !== 'undefined') {
+			for (var i = 0; i < obj.dose[0].length; i++) {
+				x = 4 * i;
+				obj.dose[0+i] = {
+					time: obj.header[0][0+x],
+					amount: obj.header[0][1+x],
+					administration: obj.header[0][2+x],
+					substance: obj.header[0][3+x],
+					form: obj.header[0][4+x] 
+				};
+			};
 		}
+
+		fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
+			if (err) throw err;
+		});
 	})
 });
