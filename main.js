f1beadb6b5ce8d4a6d28807229200707940730b5
Christian Bundy <me@christianbundy.com>
2014-01-25 21:32:28 -0800

    Remove </p> prepended to reports

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ dive(__dirname + '/json', function (err, file) {
 	fs.readFile(file, function (err, contents) {
 		if (err) throw err;
 		var obj = JSON.parse(contents);
+		var str = JSON.stringify(obj);
+		obj = JSON.parse(str.replace('"report":"</p>', '"report":"<p>'));
 		fs.writeFile(file, JSON.stringify(obj, null, 1) + "\r\n", function(err) {
 			if (err) throw err;
 		}); 
