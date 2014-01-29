eb68f0e84c481b2c680c67a871c7e72e2df834ac
Christian Bundy <me@christianbundy.com>
2014-01-29 12:31:27 -0800

    Change ground/crushed to ground

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ erowid(function (item) {
 	if ('dose' in item) {
 		for (var i = 0; i < item.dose.length; i++) {
 			if ('form' in item.dose[i]) {
-				if (item.dose[i].form === 'pill/tablet') {
-					item.dose[i].form = 'tablet';
+				if (item.dose[i].form === 'ground/crushed') {
+					item.dose[i].form = 'ground';
 				}
 			}
 		};
