31a5a2bd2e8f0513cc0b8bd14a42b41deab60ac5
Christian Bundy <me@christianbundy.com>
2014-01-29 12:43:23 -0800

    Move time to minute format

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var erowid = require('./lib/erowid.js');
 erowid(function (item) {
 	if ('dose' in item) {
 		for (var i = 0; i < item.dose.length; i++) {
-			if ('form' in item.dose[i]) {
-				if (item.dose[i].form === 'ground/crushed') {
-					item.dose[i].form = 'ground';
+			if ('time' in item.dose[i]) {
+				if (item.dose[i].time.indexOf(':') > 0) {
+					arr = item.dose[i].time.split(':');
+					item.dose[i].time = parseInt(arr[0] * 60, 10) + parseInt(arr[1], 10); 
 				}
 			}
 		};
