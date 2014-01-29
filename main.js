26747cb3b66cad439cf4bc0721e8566e9855d9be
Christian Bundy <me@christianbundy.com>
2014-01-28 20:36:04 -0800

    Use gzip compression and only expose one function

--- main.js
+++ main.js
@@ -1,29 +1,76 @@
-var fs  = require("fs");
-var cheerio = require("cheerio");
-var request = require('request');
+var fs  = require("graceful-fs");
+var zlib = require('zlib');
+var async = require('async');
 
-var erp = {
-	config: {
-		index:  0,
-		input:  './erowid.json',
-		output: './sample.json',
-		every:  false
-	},
-	main: function (item) {
-		item.author.name = 'John Dose'
-		this.write(item);
-	},
-	write: function (obj) {
-		fs.appendFileSync(erp.config.output, JSON.stringify(obj) + "\n");
-	},
-	each: function (obj) {
-		this.config.index++;
-		this.main(obj);
-	}
+var Erowid = function (main, compress) {
+	var Erowid = {
+		config: {
+			input:    '/erowid.json',
+			output:   '/sample.json',
+			gz:       '/erowid.json.gz',
+			compress: compress || false
+		},
+		data: []
+	};
+
+	// Data input handler
+	Erowid.in = function (str, fn) {
+		console.log('> output:   ' + Erowid.config.output);
+		if (Erowid.config.compress) console.log('> compress: ' + Erowid.config.gz);
+		console.log();
+		async.series([
+
+			function(seriesCb) {
+				async.each(JSON.parse(str), function (obj, eachCb) {
+					if (obj.id < 500 || Erowid.config.compress) {
+						Erowid.data.push(fn(obj) || obj);
+					}
+					eachCb(null);
+				}, function (err) {
+					seriesCb(err);
+				})
+			},
+
+			function (cb) {
+				var str = JSON.stringify(Erowid.data, null, 2);
+				var both = false;
+				fs.writeFile(__dirname + Erowid.config.output, str, function (err) {
+					if (both || !Erowid.config.compress) {
+						cb(err);
+					} else {
+						both = true;
+					}
+				});
+
+				zlib.gzip(JSON.stringify(Erowid.data), function (error, result) {
+					if (error) throw error;
+					fs.writeFile(__dirname + Erowid.config.gz, result, function (err) {
+						if (both) {
+							cb(err);
+						} else {
+							both = true;
+						}
+					})
+				});
+			}
+			]);
+	};
+
+
+	console.log('> input:    ' + Erowid.config.input);
+	fs.readFile(__dirname + Erowid.config.input, function(inputError, data){
+		if (!inputError) {
+			Erowid.in(data, main);
+		} else {
+			throw "No file at " + __dirname + Erowid.config.input;
+		}
+	});
 }
 
-fs.openSync(erp.config.output, 'w');
-fs.readFileSync(erp.config.input).toString().split("\n").forEach(function (line) {
-	if (erp.config.index < 20534 && (erp.config.every || erp.config.index < 100))
-		erp.each(JSON.parse(line));
-});
+///////////////////////////////////////////////////////////////
+
+var i = 0;
+
+Erowid( function (item) {
+	return item;
+}, true);
