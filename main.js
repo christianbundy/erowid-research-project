91a3e369fe5a07e5b4b0a61bce12ff54472221a7
Christian Bundy <me@christianbundy.com>
2014-01-29 12:46:19 -0800

    Remove null experience dates

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ var erowid = require('./lib/erowid.js');
 erowid(function (item) {
 	if ('dose' in item) {
 		for (var i = 0; i < item.dose.length; i++) {
-			if ('time' in item.dose[i]) {
-				if (item.dose[i].time.indexOf(':') > 0) {
-					arr = item.dose[i].time.split(':');
-					item.dose[i].time = parseInt(arr[0] * 60, 10) + parseInt(arr[1], 10); 
-				}
+			if ('specific' in item.dose[i]) {
+				if item.dose[i].specific === 'P. cubensis'
+					item.dose[i].specific = 'Psilocybe cubensis';
 			}
 		};
 	}
