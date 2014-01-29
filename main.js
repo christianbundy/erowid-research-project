223fa3d802f960532b21d75fdd06bd1de7dfa8d2
Christian Bundy <me@christianbundy.com>
2014-01-29 14:34:03 -0800

    Change sample size to 100

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ erowid(function (item) {
 	if ('dose' in item) {
 		for (var i = 0; i < item.dose.length; i++) {
 			if ('specific' in item.dose[i]) {
-				if item.dose[i].specific === 'P. cubensis'
+				if (item.dose[i].specific === 'P. cubensis')
 					item.dose[i].specific = 'Psilocybe cubensis';
 			}
 		};
