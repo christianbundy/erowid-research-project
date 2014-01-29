6015f662b057172232a00c516a7fd0b014f72ebd
Christian Bundy <me@christianbundy.com>
2014-01-29 12:26:04 -0800

    Replace pill/tablet with tablet

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 var erowid = require('./lib/erowid.js');
 
 erowid(function (item) {
-	return item;
+	if ('dose' in item) {
+		for (var i = 0; i < item.dose.length; i++) {
+			if ('form' in item.dose[i]) {
+				if (item.dose[i].form === 'pill/tablet') {
+					item.dose[i].form = 'tablet';
+				}
+			}
+		};
+	}
 }, true);
