27dc8f9bbf0962d6b25ebfcfb88a1980820c0f22
Christian Bundy <me@christianbundy.com>
2014-01-28 21:30:16 -0800

    Remove anonymous author code from main

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 var erowid = require('./lib/erowid.js');
 
 erowid(function (item) {
-	if ('author' in item && 'name' in item.author) {
-		if (item.author.name.toLowerCase() === 'anonymous') {
-			delete item.author.name;
-		}
-	}
-
 	return item;
 }, true);
